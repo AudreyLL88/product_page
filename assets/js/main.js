2bc83d613f67b04ac43b574ecbb9b92e5ac9b70e
AudreyLL88 <a.lloancy@gmail.com>
2021-01-08 11:34:43 +0000

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+gsap.from(".shoe-figure", { opacity: 0, duration: 1, delay: 1.5, x: -100 });
+gsap.from(".shoe-img", { opacity: 0, duration: 1, delay: 2, y: -50 });
+gsap.from(".dots", { opacity: 0, duration: 1, delay: 2.2, y: 50 });
+gsap.from(".shoe-info", { opacity: 0, duration: 1, delay: 2.5, y: -50 });
+gsap.from(".size", { opacity: 0, duration: 1, delay: 3, x: -100 });
+gsap.from(".count", { opacity: 0, duration: 1, delay: 3, x: 100 });
+gsap.from(".price-title", { opacity: 0, duration: 1, delay: 3.5, y: 50 });
+gsap.from(".price-button", { opacity: 0, duration: 1, delay: 3.5, y: -50 });
